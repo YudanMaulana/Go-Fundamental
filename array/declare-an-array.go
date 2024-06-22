083595aab7fb37aadaa05331f15836f920567302
YudanMaulana <yudanmaulana21@gmail.com>
2024-06-22 21:27:40 +0700

    with inferred lengths

--- array/declare-an-array.go
+++ array/declare-an-array.go
@@ -9,7 +9,10 @@ func main() {
 	var arr1 = [3]int{1, 2, 3}
 	// dengan Short Variable :=
 	arr2 := [5]int{4, 5, 6, 7, 8}
+	// Kita bisa tidak mendeklarasikan bilangan dengan "..."
+	arr3 := [...]int{4, 5, 6, 7, 8}
 
 	fmt.Println(arr1)
 	fmt.Println(arr2)
+	fmt.Println(arr3)
 }
